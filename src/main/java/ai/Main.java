55f548de16142cae3442d581eae1327895a4ef0a
TestVagrant-1 <TestVagrant-1@Ajay-Macbook-TestVagrant.local>
2024-03-07 12:02:12 +0530

    added zigzag convertion

---
+++ src/main/java/ai/Main.java
@@ -0,0 +1,12 @@
+package ai;
+
+import ai.leetocde.*;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        zigzagConversion zigzagConversion = new zigzagConversion();
+        System.out.println(zigzagConversion.convert("PAYPALISHIRING", 3));
+    }
+}
